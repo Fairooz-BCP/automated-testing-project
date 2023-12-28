dc959c813f869a75e7dc9083890abbdf852e246a
Fairooz-BCP <Fairooz.Jalal@BCPSoftware.com>
2023-12-28 16:44:27 +0000

    Update placeOrder.js

--- Cypress Testing/responses/placeOrder.js
+++ Cypress Testing/responses/placeOrder.js
@@ -2,9 +2,56 @@
 function mockOrderResponse(req){
 
     const custData = req;
+    let  orderID, orderNumber, depot, orderDate;
     console.log("customer data", custData);
 
-    return "order received";
+    // order number
+    const min = 100000;
+    const max = 999999;
+
+    orderNumber = String(Math.floor(Math.random() * (max - min + 1) + min));
+
+    // get current date and time
+    orderDate = getCurrentDateTime();
+    console.log(orderDate);
+    
+    // depot
+    depot = 'a01';
+
+    // create order ID - combination of depot + order number + date
+    orderID = depot + "-" + orderNumber + "-" + getDate(orderDate);
+    console.log(orderID)
+
+    // create json with response
+    let orderResponse ={
+        orderNumber : orderNumber,
+        orderID : orderID,
+        depot : depot,
+        orderDate : orderDate
+    }
+    
+    console.log(orderResponse);
+
+    // return json
+    return orderResponse;
+
+}
+
+function getCurrentDateTime(){
+
+    return new Date().toISOString().replace(/Z/, '+00:00');
+
+}
+
+function getDate(orderDate){
+
+    // converts yyyy-mm-dd to yymmdd
+    let date = new Date(orderDate);
+    let year = date.getFullYear().toString().slice(-2); 
+    let month = ('0' + (date.getMonth() + 1)).slice(-2); 
+    let day = ('0' + date.getDate()).slice(-2);
+    console.log(`${day}${month}${year}`)
+    return `${day}${month}${year}`;
 
 }
 
