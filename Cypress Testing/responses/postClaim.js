b82ac0dce7437e6b4da87239b9c35a010eb0d8d3
Fairooz-BCP <Fairooz.Jalal@BCPSoftware.com>
2024-01-08 13:45:57 +0000

    Updated to handle multiple claim lines
    Multi claim lines are validated. Returns error if any one of the claim line fields are invalid.
    Multi claim lines are created in the response matching to how many was received in the request.

--- Cypress Testing/responses/postClaim.js
+++ Cypress Testing/responses/postClaim.js
@@ -11,18 +11,68 @@
 
 // claimLines.claimLine also increments for each claim line added in response body
 
+let claimLinesArray = [];
+let claimRequest;
+let claimLineResult;
+
 
 function generateClaimResponse(req)
 {
-    // validate claim fields
-    let claimResult = validateClaimFields(req.body)
-   
+    // store claim fields
+    claimRequest = req.body.claim[0];
+    const {originalOrderID, depot, originalOrder, originalDeliveryDate,
+        originalInvoice, customerCode}= claimRequest;
     
-    let claimLineResult = validateClaimLinesFields(req.body);
+    // validate claim fields
+    let claimResult = validateClaimFields(claimRequest)
+    if( !claimResult) throw new Error('Invalid request. Claim doesn not have valid key fields.');
+    
+    // iterate over each claim line in claim request and validate
+    for (i = 0; i < claimRequest.claimLines.length; i++)
+    {
+        claimLinesArray.push(claimRequest.claimLines[i]);
+        claimLineResult = validateClaimLinesFields(claimLinesArray[i]);
+        if(!claimLineResult) throw new Error('Invalid request. Claim line does not have valid key fields.');
+    }
 
+     const min = 100000;
+     const max = 999999;
 
-    return true;
-    // validate claim lines fields
+     const claimMin = 100;
+     const claimMax = 999
+ 
+     idNumber = String(Math.floor(Math.random() * (max - min + 1) + min));
+     claimNumber = String(Math.floor(Math.random() * (claimMax - claimMin + 1) + claimMin));
+
+    // generate claim response 
+    let claimResponse ={
+        id: `${depot},${idNumber}`,
+        created: true,
+        depot : depot,
+        claimNumber : claimNumber,
+        claimLines :[]
+    };
+
+    // push claim lines into claim response
+    for(let i=1; i<= claimLinesArray.length; i++)
+    {
+        claimResponse.claimLines.push(
+            {
+                id : `${depot},${idNumber}.${i}`,
+                created : true,
+                depot : depot,
+                claimNumber : claimNumber,
+                claimLine : i
+            }
+        )
+    }
+
+    // reset claim lines array
+    claimLinesArray = [];
+
+    // return response
+    return claimResponse
+    
 }
 
 function validateClaimFields(fields)
@@ -31,6 +81,7 @@ function validateClaimFields(fields)
     const {originalOrderID, depot, originalOrder, originalDeliveryDate,
         originalInvoice, customerCode}= fields;
 
+    // validate claim fields
     if(originalOrderID)
     {
         return true;
@@ -47,8 +98,11 @@ function validateClaimFields(fields)
     {
         return true;
     }
+    else{
+        false;
+    }
 
-    throw new Error('Invalid request. None of the required field combinations match.');
+    throw new Error('Invalid request. None of the required claim line field combinations match.');
     
 }
 
@@ -58,6 +112,7 @@ function validateClaimLinesFields(fields)
     // store claim lines fields
     const {originalLineNumber, sentQuantity, reasonCode, productCode} = fields;
 
+    // validate claim lines fields
     if( originalLineNumber && sentQuantity && reasonCode)
     {
         return true;
@@ -66,11 +121,9 @@ function validateClaimLinesFields(fields)
     {
         return true;
     }
-
-    throw new Error('Invalid request. None of the required field combinations match.');
-
-    // originalLineNumber, sentQuantity and reasonCode, or
-    // productCode, sentQuantity and reasonCode
+    else{
+        return false;
+    }
 }
 
 
